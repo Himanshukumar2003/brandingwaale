d8d421dd7e88283cf3517b6f5e510cfc24288f3d
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-05-21 23:40:02 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -186,24 +186,6 @@ for (let i = 0; i < 2; i++) {
 marqueeLeft.innerHTML = leftHTML;
 marqueeRight.innerHTML = rightHTML;
 
-const tabs = document.querySelectorAll(".product-tab");
-const contents = document.querySelectorAll(".product-content");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    // Remove active class from all tabs
-    tabs.forEach((t) => t.classList.remove("active"));
-    // Add active class to clicked tab
-    tab.classList.add("active");
-
-    // Hide all product contents
-    contents.forEach((content) => content.classList.remove("active"));
-    // Show the content related to clicked tab
-    const activeContent = document.getElementById(tab.getAttribute("data-tab"));
-    if (activeContent) activeContent.classList.add("active");
-  });
-});
-
 var swiperThree = new Swiper(".app-home", {
   loop: true,
   pagination: false,
